e31979c2c9dbf04e065978a4faeef022062bf31b
Markiyan <>
2021-06-01 21:10:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,17 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "bucket3" {
   bucket = "s3-website-test-tietoevry-learn-github-exercise-2"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.bucket3.id
   source = "index.html"
   acl    = "public-read"
   key = "index.html"
